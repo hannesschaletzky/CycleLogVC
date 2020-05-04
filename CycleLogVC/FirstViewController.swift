045550b0408ee0a2f0f31deef699325f2222f177
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2020-05-05 01:36:53 +0200

    Initial Commit

---
+++ CycleLogVC/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CycleLogVC
+//
+//  Created by Hannes Schaletzky on 05.05.20.
+//  Copyright © 2020 Hannes Schaletzky. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
